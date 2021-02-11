86a38108841ecc319046e80b42ea509a98b3f686
Hansatang <portokarlos@gmail.com>
2021-02-11 10:54:34 +0100

    All OK

---
+++ src/SynchList/ListContainer.java
@@ -0,0 +1,22 @@
+package SynchList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListContainer
+{
+  private List<Integer> integerList;
+
+  public ListContainer()
+  {
+    this.integerList = new ArrayList<Integer>();
+  }
+
+  public synchronized void add(int integer){
+    integerList.add(integer);
+  }
+
+  public synchronized int getLength(){
+    return integerList.size();
+  }
+}
